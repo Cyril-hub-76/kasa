f847cf511a93bada67449b3fdeb166e9205aac12
cyril <cyril@debian>
2024-11-13 23:21:56 +0100

    Implémentation de la page error 404

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -1,7 +1,10 @@
+import "../../style/Error.scss";
 export default function Error () {
     return (
-        <div>
-            Error
+        <div className="errorContainer">
+            <h1 className="error">404</h1>
+            <p><span>Oups! La page que</span> <span>vous demandez n'existe pas.</span></p>
+            <a href="/">Retounez sur la page d'accueil</a>
         </div>
     )
 }
